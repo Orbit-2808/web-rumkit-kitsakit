f77d552d1c98375e371517b0fe435523ace81115
aryaayy <aryayyy9604@gmail.com>
2024-04-01 11:49:41 +0700

    updated db

--- database/db_rumah_sakit.sql
+++ database/db_rumah_sakit.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100432 (10.4.32-MariaDB)
  File Encoding         : 65001
 
- Date: 30/03/2024 23:44:43
+ Date: 01/04/2024 11:49:25
 */
 
 SET NAMES utf8mb4;
@@ -24,20 +24,22 @@ DROP TABLE IF EXISTS `departmen`;
 CREATE TABLE `departmen`  (
   `id_departmen` int NOT NULL AUTO_INCREMENT,
   `nama_departmen` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `spesialisasi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `lokasi` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_departmen`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of departmen
 -- ----------------------------
-INSERT INTO `departmen` VALUES (1, 'Radiology', 'Hospital Building, 2nd Floor');
-INSERT INTO `departmen` VALUES (2, 'Emergency', 'Emergency Ward');
-INSERT INTO `departmen` VALUES (3, 'Surgery', 'Operating Room 1');
-INSERT INTO `departmen` VALUES (4, 'Cardiology', 'Heart Wing');
-INSERT INTO `departmen` VALUES (5, 'Pediatrics', 'Children Ward');
-INSERT INTO `departmen` VALUES (6, 'Neurology', 'Neurological Center');
-INSERT INTO `departmen` VALUES (7, 'Orthopedics', 'Orthopedic Wing');
+INSERT INTO `departmen` VALUES (1, 'Radiology', 'Radiologist', 'Hospital Building, 2nd Floor');
+INSERT INTO `departmen` VALUES (2, 'Emergency', 'ER Specialist', 'Emergency Ward');
+INSERT INTO `departmen` VALUES (3, 'Surgery', 'Surgeon', 'Operating Room 1');
+INSERT INTO `departmen` VALUES (4, 'Cardiology', 'Cardiologist', 'Heart Wing');
+INSERT INTO `departmen` VALUES (5, 'Pediatrics', 'Pediatric Surgeon', 'Children Ward');
+INSERT INTO `departmen` VALUES (6, 'Neurology', 'Neurologist', 'Neurological Center');
+INSERT INTO `departmen` VALUES (7, 'Orthopedics', 'Orthopedic Surgeon', 'Orthopedic Wing');
+INSERT INTO `departmen` VALUES (9, 'ENT', 'ENT Specialist', 'ENT building');
 
 -- ----------------------------
 -- Table structure for dokter
@@ -46,23 +48,24 @@ DROP TABLE IF EXISTS `dokter`;
 CREATE TABLE `dokter`  (
   `id_dokter` int NOT NULL AUTO_INCREMENT,
   `nama_dokter` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `spesialisasi` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jenis_kelamin_dokter` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `alamat_dokter` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `telefon_dokter` int NOT NULL,
+  `telefon_dokter` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_departmen` int NOT NULL,
   PRIMARY KEY (`id_dokter`) USING BTREE,
   INDEX `id_departmen`(`id_departmen` ASC) USING BTREE,
   CONSTRAINT `dokter_ibfk_1` FOREIGN KEY (`id_departmen`) REFERENCES `departmen` (`id_departmen`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of dokter
 -- ----------------------------
-INSERT INTO `dokter` VALUES (2, 'Ayay Polynomial', 'Neurologist', 'P', 'Akihabara, Japan', 2147483647, 'doctors-1.jpg', 6);
-INSERT INTO `dokter` VALUES (3, 'Iduy Flutter', 'Psychiatrist', 'L', 'Britannia, Holy Britannian Empire', 2147483647, 'doctors-2.jpg', 2);
-INSERT INTO `dokter` VALUES (4, 'Alal Hadoop', 'Orthopedic Surgeon', 'L', 'Mars, Tijuana, Earth', 2147483647, 'doctors-3.jpg', 7);
+INSERT INTO `dokter` VALUES (2, 'Ayay Polynomial', 'P', 'Akihabara, Japan', '081384758358', 'doctors-1.jpg', 6);
+INSERT INTO `dokter` VALUES (3, 'Iduy Flutter', 'L', 'Britannia, Holy Britannian Empire', '082917483746', 'doctors-2.jpg', 2);
+INSERT INTO `dokter` VALUES (4, 'Alal Hadoop', 'L', 'Mars, Tijuana, Earth', '082389298348', 'doctors-3.jpg', 7);
+INSERT INTO `dokter` VALUES (11, 'Dimas Anjaymabar', 'L', 'Depok, Indonesia', '081283234345', 'doctors-4.jpg', 1);
+INSERT INTO `dokter` VALUES (16, 'sep asep', 'L', 'Bandung, Indonesia', '081282220576', 'doctors-5.jpg', 9);
 
 -- ----------------------------
 -- Table structure for jadwal_praktek
@@ -120,19 +123,20 @@ CREATE TABLE `obat`  (
   `nama_obat` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `deskripsi` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `harga` decimal(10, 2) NULL DEFAULT NULL,
+  `stok` int NOT NULL,
   PRIMARY KEY (`id_obat`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of obat
 -- ----------------------------
-INSERT INTO `obat` VALUES (1, 'Parasetamol', 'Untuk mengatasi demam dan sakit', 20000.00);
-INSERT INTO `obat` VALUES (2, 'Ibuprofen', 'Untuk mengatasi rasa sakit, peradangan, dan demam', 40000.00);
-INSERT INTO `obat` VALUES (3, 'Omeprazol', 'Untuk meredakan sakit maag dan asam lambung', 99000.00);
-INSERT INTO `obat` VALUES (4, 'Amoksisilin', 'Antibiotik untuk infeksi bakteri', 119900.00);
-INSERT INTO `obat` VALUES (5, 'Loratadin', 'Untuk alergi dan demam hay', 599900.00);
-INSERT INTO `obat` VALUES (6, 'Aspirin', 'Untuk meredakan rasa sakit dan melancarkan peredaran darah', 499000.00);
-INSERT INTO `obat` VALUES (7, 'Simvastatin', 'Untuk menurunkan kadar kolesterol', 299900.00);
+INSERT INTO `obat` VALUES (1, 'Parasetamol', 'Untuk mengatasi demam dan sakit', 20000.00, 30);
+INSERT INTO `obat` VALUES (2, 'Ibuprofen', 'Untuk mengatasi rasa sakit, peradangan, dan demam', 40000.00, 20);
+INSERT INTO `obat` VALUES (3, 'Omeprazol', 'Untuk meredakan sakit maag dan asam lambung', 99000.00, 45);
+INSERT INTO `obat` VALUES (4, 'Amoksisilin', 'Antibiotik untuk infeksi bakteri', 119900.00, 24);
+INSERT INTO `obat` VALUES (5, 'Loratadin', 'Untuk alergi dan demam hay', 599900.00, 12);
+INSERT INTO `obat` VALUES (6, 'Aspirin', 'Untuk meredakan rasa sakit dan melancarkan peredaran darah', 499000.00, 24);
+INSERT INTO `obat` VALUES (7, 'Simvastatin', 'Untuk menurunkan kadar kolesterol', 299900.00, 29);
 
 -- ----------------------------
 -- Table structure for pasien
@@ -218,14 +222,15 @@ CREATE TABLE `rekam_medis`  (
   INDEX `id_pasien`(`id_pasien` ASC) USING BTREE,
   CONSTRAINT `rekam_medis_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `rekam_medis_ibfk_2` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of rekam_medis
 -- ----------------------------
-INSERT INTO `rekam_medis` VALUES (1, '2023-03-11 08:00:00', 'Osteoartritis', 'Rutin untuk terapi dan jangan lupa untuk meminum obat yang di berikan', 1, 1);
-INSERT INTO `rekam_medis` VALUES (2, '2023-03-11 08:20:00', 'Rinitis Akut', 'Istirahat yang cukup dan banyak minum air putih', 1, 2);
+INSERT INTO `rekam_medis` VALUES (1, '2023-03-11 08:00:00', 'Osteoartritis', 'Rutin untuk terapi dan jangan lupa untuk meminum obat yang di berikan', 16, 1);
+INSERT INTO `rekam_medis` VALUES (2, '2023-03-11 08:20:00', 'Rinitis Akut', 'Istirahat yang cukup dan banyak minum air putih', 16, 2);
 INSERT INTO `rekam_medis` VALUES (3, '2023-03-16 09:12:00', 'Hipersensitivitas Dentin', 'Jangan mengkonsumsi makanan atau minuman yang terlalu panas dan dingin atau rasa yang kuat', 2, 3);
+INSERT INTO `rekam_medis` VALUES (4, '2024-03-04 00:00:00', 'Keseleo', 'Hati-hati ketika berjalan', 4, 4);
 
 -- ----------------------------
 -- Table structure for resep_obat
@@ -234,7 +239,7 @@ DROP TABLE IF EXISTS `resep_obat`;
 CREATE TABLE `resep_obat`  (
   `id_resep_obat` int NOT NULL AUTO_INCREMENT,
   `jumlah` int NOT NULL,
-  `intruksi` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `instruksi` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_rekam_medis` int NOT NULL,
   `id_obat` int NOT NULL,
   PRIMARY KEY (`id_resep_obat`) USING BTREE,
@@ -242,7 +247,7 @@ CREATE TABLE `resep_obat`  (
   INDEX `id_obat`(`id_obat` ASC) USING BTREE,
   CONSTRAINT `resep_obat_ibfk_1` FOREIGN KEY (`id_rekam_medis`) REFERENCES `rekam_medis` (`id_rekam_medis`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `resep_obat_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of resep_obat
@@ -250,5 +255,6 @@ CREATE TABLE `resep_obat`  (
 INSERT INTO `resep_obat` VALUES (1, 12, '3x sehari', 1, 4);
 INSERT INTO `resep_obat` VALUES (2, 6, '2x sehari', 2, 1);
 INSERT INTO `resep_obat` VALUES (3, 15, '3x sehari', 3, 2);
+INSERT INTO `resep_obat` VALUES (4, 5, '3x sehari', 1, 7);
 
 SET FOREIGN_KEY_CHECKS = 1;
