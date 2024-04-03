43a1163ceeb3dcd1aa157191b2b8a715fff7c7b7
aryaayy <aryayyy9604@gmail.com>
2024-04-03 23:12:42 +0700

    kelupaan

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
   <section id="hero" class="d-flex align-items-center">
     <div class="container">
       <h1>Welcome to KitSakit</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <h2>Sini abang obatin</h2>
       <a href="#about" class="btn-get-started scrollto">Get Started</a>
     </div>
   </section><!-- End Hero -->
